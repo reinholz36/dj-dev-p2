bf080b389835b0511727f7a939c975316bb7c99f
reinholz36 <dreinholz28@gmail.com>
2022-03-04 16:46:36 -0500

    updated edit bug, now functions but doesn't work with dropdown menu on example-details page

--- public/assets/js/example.js
+++ public/assets/js/example.js
@@ -3,6 +3,7 @@ const $exampleText = $('#example-text');
 const $exampleDescription = $('#example-description');
 const $submitBtn = $('#submit');
 const $exampleList = $('#example-list');
+const $detailExampleList = $('#detail-example-list')
 const $emojiValue = $('#emoji');
 
 const editText = document.getElementById('editText');
@@ -29,14 +30,19 @@ const API = {
     });
   },
   editDream: function (id) {
+    let url = window.location;
+    url.toString().slice(0, 22)
+    // console.log(url)
+    // console.log('test value',editEmoji.sel.options[sel.selectedIndex].text)
     return $.ajax({
-      url: 'api/example/' + id,
+      //change splice number to 22 for localhost dev or 41 for live site
+      url: url.toString().slice(0, 41) + 'api/example/' + id,
       type: 'PUT',
 
       data: {
         "text":editText.innerHTML, 
         "description":editDescription.innerHTML,
-        "emoji":editEmoji.innerHTML,
+        "emoji":editEmoji.innerText,
       }
     });
   }, 
@@ -55,7 +61,7 @@ const refreshExamples = function () {
       var createdAttime = moment.utc(example.createdAt).format("MM/DD/YYYY");
       const $a = $('<a class="pastjournalentrymarker">')
         .text(createdAttime + " " + example.text + " " + example.emoji)
-
+        .attr('href', '/example/' + example.id);
         
         const $li = $('<li>')
         .attr({
@@ -78,12 +84,12 @@ const refreshExamples = function () {
         // $li.append($editbutton);
 
         // expand button
-        const $expandbutton = $('<a>')
-        .html('<i class="fa-solid fa-expand"></i>')
-        .attr({
-          class: 'btn btn-dark text-white pastjournalentrymarker float-right',
-          "href": '/example/' + example.id});
-      $li.append($expandbutton);
+      //   const $expandbutton = $('<a>')
+      //   .html('<i class="fa-solid fa-expand"></i>')
+      //   .attr({
+      //     class: 'btn btn-dark text-white pastjournalentrymarker float-right',
+      //     "href": '/example/' + example.id});
+      // $li.append($expandbutton);
 
       //     // line break
       // const $linebreak = $('<br>')
@@ -169,7 +175,7 @@ const handleEditBtnClick = function () {
   const idToEdit = $(this).parent().attr('data-id');
 
   API.editDream(idToEdit).then(function () {
-    refreshExamples();
+    $(document.location).attr('href', '/example');
   });
 };
 
@@ -185,5 +191,5 @@ const handleDeleteBtnClick = function () {
 
 // Add event listeners to the submit and delete buttons
 $submitBtn.on('click', handleFormSubmit);
-$exampleList.on('click', '.editDream', handleEditBtnClick);
+$detailExampleList.on('click', '.editDream', handleEditBtnClick);
 $exampleList.on('click', '.delete', handleDeleteBtnClick);
